53fe8baeef101cb3d3945e6adc9fb65a0cb865b9
albertlunde132 <albert.lunde@gmail.com>
2020-03-31 13:57:02 +0200

    first commit!

---
+++ log.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Scraping Projekt. I dette dokument, vil jeg logge hvad jeg laver hver dag i mit
+lille scraping projekt. 
+
+31-03-2020::
+    Jeg har installet python og kite på min nye PC og forbundet en mappe til github. 
+    Planen er at lave de første øvelser fra Scraping-bogen og derefter at lave et nyt repo
+    på github, hvor jeg kan smide alt det her over i. Det kan desværre godt være at jeg
+    bliver nødt til at pushe fra min git-bash, da jeg endnu ikke har fundet en måde
+    at gøre det på igennem Spyder.
+    
+"""
+
